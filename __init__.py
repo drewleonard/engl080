f4b3e62999e94ce746b99462d1f2a338dde73067
drewleonard <drew.n.leonard.19@dartmouth.edu>
2018-11-12 15:19:30 -0500

    about page progress

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ def play():
 
     # Set corpus headline source
 	# options: nyt, breitbart, cnn, fox, buzzfeed
-	corpus_headlines = "breitbart.dill"
+	corpus_headlines = "fox.dill"
 
 	# Load corpus object into memory and set headlines
 	corpus_obj = corpus.Corpus(path='corpus_small.p')
@@ -29,7 +29,6 @@ def play():
 	generator_obj = generator.Generator(corpus_obj.headlines)
 
 	# Load game object into memory
-	# game_obj = game.Game(generator_obj, seed_headlines)
 	game_obj = game.Game(generator_obj, corpus_obj.headlines)
 
 	return render_template('play.html', inputs=json.dumps(game_obj.inputs))
